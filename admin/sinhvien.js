4a88ecf8bf9d99c7dcd16e9a04804ac965369074
thang <thang13199@gmail.com>
2019-12-23 07:51:33 +0700

    Merge remote-tracking branch 'origin/diep' into HEAD # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- admin/sinhvien.js
+++ admin/sinhvien.js
@@ -46,40 +46,7 @@ $('#confirmImportModal').on('click',function (e) {
 })
 
 
-async function uploadfile() { //login vao app
-    var formData=new FormData();
-    formData.append('students',document.getElementById('customFile').files[0]);
-    const config = {
-        headers: {
-            'token': window.localStorage.getItem('token'),
-            'content-type': 'application/x-www-form-urlencoded'
-        },
-            onUploadProgress: function(progressEvent) {
-            var percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total)
-            console.log(percentCompleted)
-        }
-    }
-    axios.post('http://localhost:5000/api/v1/students/',
-        formData
-        ,
-        config
-    )
-        .then(function (response) {
-            if (response.data.success===true) {
-                alert('Them thanh cong')
 
-            }
-            else {
-                console.log(response.data.reason);
-                alert(response.data.reason)
-
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-}
 
 
 
