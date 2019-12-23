4260a58fbb8c6ae6d899c4e432309b94238deb25
diepninh <ninhhongdiep99@gmail.com>
2019-12-23 13:27:25 +0700

    22

--- user/dangki.js
+++ user/dangki.js
@@ -16,33 +16,13 @@ let danhsachmondangki = [{
     date:"24/12/2019",
     place:"P302-G2"
 }];
-// let regisTable = $('#regisTable')[0];
-// for(let i=0; i<danhsachmondangki.length; i++){
-//     let row = regisTable.insertRow(-1);
-//     let cell1 = row.insertCell(-1);
-//     let cell2 = row.insertCell(-1);
-//     let cell3 = row.insertCell(-1);
-//     let cell4 = row.insertCell(-1);
-//     let cell5 = row.insertCell(-1);
-//     let cell6 = row.insertCell(-1);
-//     let cell7 = row.insertCell(-1);
-//     let cell8 = row.insertCell(-1);
-//     cell1.innerHTML = i+1;
-//     cell2.innerHTML = danhsachmondangki[i].id_course;
-//     cell3.innerHTML = danhsachmondangki[i].course_name;
-//     cell4.innerHTML = danhsachmondangki[i].portal;
-//     cell5.innerHTML = danhsachmondangki[i].time;
-//     cell6.innerHTML = danhsachmondangki[i].date;
-//     cell7.innerHTML = danhsachmondangki[i].place;
-//     cell8.innerHTML = "<input type='checkbox'>";
-//
-// }
+
 
 
 addmoreRows()
 
 function addmoreRows() {
-    $('#regisTable > tbody:last-child').append('<tr class="subRow"><td>1</td><td>INT33023</td><td>Phat trien ung dung web</td><td>3</td>' +
+    $('#mainTable > tbody:last-child').append('<tr class="subRow"><td>1</td><td>INT33023</td><td>Phat trien ung dung web</td><td>3</td>' +
         '<td> <select class="custom-select custom-select-sm form-control form-control-sm">\n' +
         '                            <option>10</option>\n' +
         '                            <option>15</option>\n' +
@@ -59,6 +39,10 @@ function addmoreRows() {
         '                            <option>20</option>\n' +
         '                        </select></td>'+
         '<td> <input type="checkbox"></td>' +
+        '</tr>');
+    $('#regisTable > tbody:last-child').append('<tr class="subRow"><td>1</td><td>INT33023</td><td>Phat trien ung dung web</td><td>3</td>' +
+        '<td> 8:00 SA</td><td>24/12/2019</td><td>P303-G2</td>'+
+        '<td> <i class="fas fa-trash-alt"></i></td>' +
         '</tr>');
 
 }
--- user/indangki.js
+++ user/indangki.js
@@ -1,10 +1,34 @@
 function myslide() {
     document.getElementById("slidebar").classList.toggle('active');
 }
+function exportTableToExcel(tableID, filename = ''){
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+
+    // Specify file name
+    filename = filename?filename+'.xls':'dulieu.xls';
+
+    // Create download link element
+    downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['\ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
 
 
+        downloadLink.download = filename;
 
 
-
-
+        downloadLink.click();
+    }
+}
 
